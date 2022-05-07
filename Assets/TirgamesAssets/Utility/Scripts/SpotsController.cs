5f33564ed2f920b05732d14b5b04cc3495540f42
BRIAN HAN <hanyiduo31@126.com>
2022-05-08 04:35:02 +0800

    演唱会特效 效

---
+++ Assets/TirgamesAssets/Utility/Scripts/SpotsController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotsController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<GameObject> lights;
+    public bool on = true;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void turn()
+    {
+        on = !on;
+        if (on)
+        {
+            for(int i=0; i<lights.Count; i++)
+            {
+                lights[i].SetActive(true);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < lights.Count; i++)
+            {
+                lights[i].SetActive(false);
+            }
+        }
+    }
+}
